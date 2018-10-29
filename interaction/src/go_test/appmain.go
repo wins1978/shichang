9ef8e40375d7f5b72cccea6b8cff31b6b3b5cbbd
wins1978 <2071227188@qq.com>
2018-10-30 00:57:43 +0800

    d

--- interaction/src/go_test/appmain.go
+++ interaction/src/go_test/appmain.go
@@ -3,12 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var i int
-	var j int
-	i = 10
-	j = 20
-	var k int
-	k = i * j
-	fmt.Sprintln(k)
-	Fmtdemo()
+    fmt.Println("-->first")
+    Fmtdemo()
 }

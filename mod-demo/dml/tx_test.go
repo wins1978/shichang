47b10cb0c5ded5108c9ba1e25fef90c77cda942f
wins <117076744@qq.com>
2018-11-04 17:38:33 +0800

    d

--- mod-demo/dml/tx_test.go
+++ mod-demo/dml/tx_test.go
@@ -1,12 +1,31 @@
 package dml_test
 
-import "testing"
+import (
+	"testing"
+	"fmt"
 
-//import "g.demo/db"
+	"g.demo/db"
+	"g.demo/dml"
+	"time"
+)
 
-//事物测试
+//事务测试
 
 func TestTran(t *testing.T) {
-	//da := db.InitDB()
+	
 
+	for i:=0;i<10;i++ {
+		da := db.InitDB()
+		tx := da.Begin()
+		go dml.TxInsertDept(tx)
+		go dml.TxInsertUser(tx)
+		time.Sleep(time.Duration(2) * time.Second)
+		if(tx.Error != nil) {
+		fmt.Println("---------rollback")
+		tx.Rollback()
+		}else{
+			fmt.Println("---------commit")
+			tx.Commit()
+		}
+	}
 }

345c4b030e71a7944a5d080acb4988a35801fe11
wins1978 <2071227188@qq.com>
2018-11-08 02:55:11 +0800

    d

--- src/fiorm-exapmle/query_test.go
+++ src/fiorm-exapmle/query_test.go
@@ -16,9 +16,27 @@ func TestGetItemWhereFirst(t *testing.T) {
 	var user model.User
 
 	var qstr = "name = ? or email = ?"
-	query := fiorm.Where(qstr, "wins1", "111")
+	query := fiorm.Where(qstr, "wins1", "111").
+		Select("name, address, id").
+		OrderBy("name desc")
 
-	fiorm.DB.GetItemWhereFirst(&user, query)
+	fiorm.GetItemWhereFirst(&user, query)
 
 	fmt.Println(user.ID)
+}
+
+func TestGetItemWhere(t *testing.T) {
+	var users []model.User
+
+	var qstr = "name = ? or email = ?"
+	query := fiorm.Where(qstr, "wins1", "111").
+		Select("name, address, id").
+		OrderBy("name desc").
+		Limit(10)
+
+	var cnt int64
+	fiorm.GetItemWhere(&users, query).Count(&cnt)
+
+	fmt.Println(users)
+	fmt.Println("--cnt:", cnt)
 }
